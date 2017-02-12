59ee2c7a2a5fcc9a53d529798dd1c68732ac80ec
Giacomo Tomolillo <giacomo.tomolillo@gmail.com>
2017-02-12 12:53:12 +0000

    Strong Name Key and Assembly Properties

--- DataReaderMapper/Properties/AssemblyInfo.cs
+++ DataReaderMapper/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("DataAdapterMapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DataReaderMapper")]
+[assembly: AssemblyDescription("Use AutoMapper to Map an IDataReader to an object")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataAdapterMapper")]
+[assembly: AssemblyCompany("Giacomo Tomolillo")]
+[assembly: AssemblyProduct("DataReaderMapper")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,12 +18,12 @@ using System.Runtime.InteropServices;
 // COM, impostare su true l'attributo ComVisible per tale tipo.
 [assembly: ComVisible(false)]
 
-// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+// Se il progetto viene espostso a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
 [assembly: Guid("c1715a4b-f4b2-46e8-86ed-7b39899d507a")]
 
 // Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
 //
-//      Versione principale
+//      Versione principales
 //      Versione secondaria 
 //      Numero di build
 //      Revisione
